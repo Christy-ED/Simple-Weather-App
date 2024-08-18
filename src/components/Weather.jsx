cf8f08fbfac74935a8e7cda5f615f6a4cc89db5e
Christy <Edgardchristianne@gmail.com>
2024-08-18 15:35:34 -0400

    Calling the weather API

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -11,6 +11,30 @@ import humidity_icon from '../assets/humidity.png'
 
 
 const Weather = () => {
+
+  // this functon will store the data coming from the API
+
+    // this function make a API request by city name
+    const searc = async(city) =>{
+      try{
+        // create an URL to make the API call 
+        const url = `https://api.openweathermap.org/data/2.5/weather?q= ${city}&appid=${import.meta.env.VITE_API_ID}`;
+      
+        //Fetch API to get the data from the URL
+        const response = await fetch(url);
+        const data = await response.json(); // convert the response to json
+        console.log(data); // will print the data in the console.
+      } catch(error){
+  
+      }
+    }
+  
+    useEffect(() =>{
+      search("Drexel Hill");
+    }, []) 
+
+
+
   return (
     <div className='weather'>
         <div className="search-bar">
